1302b3db7f40a5bb5ca6506294a4ee6159a12696
guilherme.anjos <anjos.guilherme.estagiario@tre-rn.jus.br>
2023-06-05 18:09:48 -0300

    estilizando a página "board"

--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import boardApi from '../api/boards.api'
-
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined'
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
+import {EmojiPIcker} from 'emoji-mart'
+import { Box, IconButton, TextField } from '@mui/material'
 
 const Board = () => {
   const {boardId} = useParams()
@@ -10,8 +14,6 @@ const Board = () => {
   const [sections, setSections] = useState([])
   const [isFavourite, setIsFavourite] = useState(false)
   const [icon, setIcon] = useState('')
-
-  console.log(boardId)
 
   useEffect(() => {
     const getBoard = async () => {
@@ -23,6 +25,7 @@ const Board = () => {
         setIsFavourite(res.favourite)
         setIcon(res.icon)
         console.log(res)
+        console.log(isFavourite)
       } catch (err) {
         alert(err)
       }
@@ -30,7 +33,54 @@ const Board = () => {
     getBoard()
   }, [boardId])
   return (
-    <div>Board</div>
+    <>
+      <Box sx={{
+        display:'flex',
+        alignItems:'center',
+        justifyContent: 'center',
+        width:'100%',
+      }}>
+        <IconButton variant ='outlined'>
+          {
+            isFavourite ?(
+              <StarOutlinedIcon color='warning'/>
+            ):(
+              <StarBorderOutlinedIcon/>
+            )
+          }
+        </IconButton>
+          <IconButton variant='outlined' color='error'>
+          <DeleteOutlinedIcon />
+
+          </IconButton>
+      </Box>
+      <Box sx={{padding: '10px 50px',}}>
+          {/* {emoji picker} */}
+      </Box>
+      <TextField
+        value={title}
+        placeholder='Untitled'
+        variant='outlined'
+        fullWidth
+        sx={{
+          '& .MuiOutlinedInput-input':{padding:0},
+          '& .MuiOutlinedInput-notchedOutline':{border:'unset'},
+          '& .MuiOutlinedInput-root':{fontSize:'2rem', fontWeight:'700'},
+        }}>
+      </TextField>
+      <TextField
+        value={description}
+        placeholder='Add a description'
+        variant='outlined'
+        multiline
+        fullWidth
+        sx={{
+          '& .MuiOutlinedInput-input':{padding:0},
+          '& .MuiOutlinedInput-notchedOutline':{border:'unset'},
+          '& .MuiOutlinedInput-root':{fontSize:'0.8 rem'},
+        }}>
+      </TextField>
+    </>
   )
 }
 
